65a4d9586eee651e4c2b2f744feb9432ac13c4a3
Rajesh Bakade <rajesh.bakade65@gmail.com>
2019-07-31 19:42:06 +0530

    file for count sum of matching array elements
    file for count sum of matching array elements

---
+++ count_sum_of_matching_array_elements.php
@@ -0,0 +1,31 @@
+<?php
+
+function sumValue($arr, $sum)
+{
+    // Initialize result 
+    $count  = 0;
+    $n      = count($arr);
+    $result = array();
+    // Consider all possible pairs  
+    // and check their sums 
+    for ($i = 0; $i < $n; $i++)
+        for ($j = $i + 1; $j < $n; $j++)
+            if (!array_key_exists('{ '.$arr[$i] . '+' . $arr[$j].' }', $result) && !array_key_exists('{ '.$arr[$j] . '+' . $arr[$i].' }', $result)) {
+                if ($arr[$i] + $arr[$j] == $sum) {
+                    $result['{ '.$arr[$i] . '+' . $arr[$j].' }'] = array(
+                        $arr[$i],
+                        $arr[$j]
+                    );
+                }
+            }
+    
+    return [count($result), implode(', ',array_keys($result))];
+}
+
+// Driver code to test above 
+$arr = array(0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,3,2,1);
+
+$len = sizeof($arr);
+$result = sumValue($arr, 6);
+echo "Total number of matching sum elements: ".$result[0].PHP_EOL;
+echo "Elements: ".$result[1];
