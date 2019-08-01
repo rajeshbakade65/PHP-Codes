25adaa82cda9bba73c87b5c69d7a33b9c3dde6dd
Rajesh Bakade <rajesh.bakade65@gmail.com>
2019-08-01 23:14:14 +0530

    Update count_number_of_characters.php

--- count_number_of_characters.php
+++ count_number_of_characters.php
@@ -9,3 +9,18 @@ for($i=0;$i<strlen($test);$i++){
     }
 }
 print_r($array);
+
+//output
+Array
+(
+    [a] => 8
+    [b] => 1
+    [d] => 10
+    [s] => 7
+    [k] => 8
+    [w] => 2
+    [p] => 4
+    [o] => 2
+    [e] => 2
+    [q] => 1
+)
