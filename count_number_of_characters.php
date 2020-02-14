a3976549834788f27d8fb74c6cb7766d2bc07437
Rajesh Bakade <rajesh.bakade65@gmail.com>
2020-02-14 12:11:31 +0530

    updated description and formatted code

--- count_number_of_characters.php
+++ count_number_of_characters.php
@@ -1,14 +1,20 @@
+Here, we need to extract each character in sepearate array as key and count as value in front of each key.
+
 <?php
-$test = 'abadsadddkdsadksawpoekwpeqpkpdoksakdsakdsdksa';
-$array = array();
-for($i=0;$i<strlen($test);$i++){
-    if(!array_key_exists($test[$i],$array)){
-        $array[$test[$i]]=1;
+
+$str   = 'abadsadddkdsadksawpoekwpeqpkpdoksakdsakdsdksa';
+$arr   = array();
+$str_chr_length = strlen($str);
+
+for ($i = 0; $i < $str_chr_length; $i++) {
+    
+    if (!array_key_exists($str[$i], $arr)) {
+        $arr[$str[$i]] = 1;
     } else {
-        $array[$test[$i]]++;
+        $arr[$str[$i]]++;
     }
 }
-print_r($array);
+print_r($arr);
 
 //output
 Array
@@ -24,3 +30,12 @@ Array
     [e] => 2
     [q] => 1
 )
+?>
+
+So, first we've defined a varible <pre>$str</pre> which contains random characters.
+Then, in the for loop we've checked if blank array conains that character. If not then add it into an array as key and initialize it to 1
+as value initally.
+If that character is already exists then it will go inside else block and will increase that characters count by 1.
+
+This way it will loop through all the characters present in the string. And, then it will add all the characters in the blank as key 
+and count of that character as value of eack key.
